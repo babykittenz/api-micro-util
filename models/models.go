9e10e9a2d78de28f0952699a7384be2aaabd7590
babykittenz <kidbymichaelb@gmail.com>
2025-03-29 11:41:29 -0500

    fix: add missing models

--- models/models.go
+++ models/models.go
@@ -1,5 +1,20 @@
 package models
 
+import "time"
+
+// User represents a system user with authentication and access permissions.
+type User struct {
+	ID         string `json:"id"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	Email      string `json:"email"`
+	Phone      string `json:"phone"`
+	Role       string `json:"role"`
+	CompanyID  string `json:"company_id"`
+	RegionID   string `json:"region_id"`
+	LocationID string `json:"location_id"`
+}
+
 // Trainee represents the details of an individual undergoing training within the system.
 type Trainee struct {
 	ID                    string `json:"id"`
@@ -24,20 +39,199 @@ type Trainee struct {
 	CheckinID             string `json:"checkin_id"`
 }
 
-type User struct{}
+// Training represents a record of a training session completed by a trainee.
+type Training struct {
+	ID             string    `json:"id"`
+	PDF            string    `json:"pdf,omitempty"`
+	DateCompleted  time.Time `json:"date_completed"`
+	ManualAddition bool      `json:"manual_addition,omitempty"`
+	TraineeID      string    `json:"trainee_id,omitempty"`
+	LocationID     string    `json:"location_id,omitempty"`
+	RegionID       string    `json:"region_id,omitempty"`
+	CompanyID      string    `json:"company_id,omitempty"`
+}
 
-type Training struct{}
+// Checkin represents a record of when a trainee checks in to a location.
+type Checkin struct {
+	ID         string    `json:"id"`
+	TraineeID  string    `json:"trainee_id,omitempty"`
+	LocationID string    `json:"location_id,omitempty"`
+	RegionID   string    `json:"region_id,omitempty"`
+	CompanyID  string    `json:"company_id,omitempty"`
+	Timestamp  time.Time `json:"timestamp"`
+	Type       string    `json:"type,omitempty"`
+}
 
-type Location struct{}
+// Company represents the details of a company within the system.
+type Company struct {
+	ID                string `json:"id"`
+	Name              string `json:"name"`
+	Address           string `json:"address"`
+	City              string `json:"city"`
+	State             string `json:"state"`
+	Zip               string `json:"zip"`
+	StorageName       string `json:"storage_name"`
+	Email             string `json:"email"`
+	Phone             string `json:"phone"`
+	Logo              string `json:"logo"`
+	BlankPDF          string `json:"blank_pdf"`
+	Map               string `json:"map"`
+	Description       string `json:"description"`
+	PreferredLanguage string `json:"preferred_language"`
+	HasAgreement      bool   `json:"has_agreement"`
+	Agreement         string `json:"agreement,omitempty"`
+	HasVideo          bool   `json:"has_video"`
+	Video             string `json:"video,omitempty"`
+}
 
-type Region struct{}
+// Region represents a geographic or operational region associated with a company.
+type Region struct {
+	ID                string   `json:"id"`
+	Name              string   `json:"name"`
+	Email             string   `json:"email"`
+	Phone             string   `json:"phone"`
+	CompanyID         string   `json:"company_id"`
+	StorageName       string   `json:"storage_name"`
+	Address           string   `json:"address"`
+	City              string   `json:"city"`
+	State             string   `json:"state"`
+	Zip               string   `json:"zip"`
+	Map               string   `json:"map"`
+	Logo              string   `json:"logo"`
+	BlankPDF          string   `json:"blank_pdf"`
+	Description       string   `json:"description,omitempty"`
+	PreferredLanguage string   `json:"preferred_lang"`
+	HasAgreement      bool     `json:"has_agreement"`
+	Agreement         string   `json:"agreement,omitempty"`
+	HasVideo          bool     `json:"has_video"`
+	Video             string   `json:"video,omitempty"`
+	Languages         []string `json:"languages"`
+}
 
-type Company struct{}
+// Location represents a specific site or facility within a region.
+type Location struct {
+	ID                             string   `json:"id"`
+	CompanyID                      string   `json:"company_id"`
+	RegionID                       string   `json:"region_id"`
+	Name                           string   `json:"name"`
+	StorageName                    string   `json:"storage_name"`
+	Phone                          string   `json:"phone"`
+	Email                          string   `json:"email"`
+	Address                        string   `json:"address"`
+	State                          string   `json:"state"`
+	City                           string   `json:"city"`
+	Zip                            string   `json:"zip"`
+	CheckinTextMessages            bool     `json:"checkin_text_messages"`
+	TextNotificationsNumber        string   `json:"text_notifications_number,omitempty"`
+	Map                            string   `json:"map"`
+	Logo                           string   `json:"logo"`
+	BlankPDF                       string   `json:"blank_pdf"`
+	Description                    string   `json:"description,omitempty"`
+	PreferredLanguage              string   `json:"preferred_lang"`
+	HasAgreement                   bool     `json:"has_agreement"`
+	Agreement                      string   `json:"agreement,omitempty"`
+	ExpirationTime                 string   `json:"expiration_time"`
+	HasVideo                       bool     `json:"has_video"`
+	Video                          string   `json:"video,omitempty"`
+	Languages                      []string `json:"languages"`
+	MshaID                         string   `json:"msha_id,omitempty"`
+	DateOfTrainingPlaceholder      string   `json:"date_of_training_placeholder"`
+	TraineeNamePlaceholder         string   `json:"trainee_name_placeholder"`
+	PhonePlaceholder               string   `json:"phone_placeholder"`
+	TrainingLocationPlaceholder    string   `json:"training_location_placeholder"`
+	EmailPlaceholder               string   `json:"email_placeholder"`
+	MshaNumberPlaceholder          string   `json:"msha_number_placeholder"`
+	ScalehouseAttendantPlaceholder string   `json:"scalehouse_attendant_placeholder"`
+	TruckNumberPlaceholder         string   `json:"truck_number_placeholder"`
+	TrainingPerformedPlaceholder   string   `json:"training_performed_placeholder"`
+	CompanyPlaceholder             string   `json:"company_placeholder"`
+}
 
-type Language struct{}
+// Language represents all the text strings used for UI localization and customization.
+type Language struct {
+	WelcomeText                   string `json:"welcome_text"`
+	WelcomeText2                  string `json:"welcome_text_2"`
+	BottomText                    string `json:"bottom_text"`
+	StartTrainingButtonText       string `json:"start_training_button_text"`
+	WhosTrainingWelcomeText       string `json:"whos_training_welcome_text"`
+	WhosTrainingWelcomeSubtext    string `json:"whos_training_welcome_subtext"`
+	FormFirstName                 string `json:"form_first_name"`
+	FormFirstNamePlaceholder      string `json:"form_first_name_placeholder"`
+	FormLastName                  string `json:"form_last_name"`
+	FormLastNamePlaceholder       string `json:"form_last_name_placeholder"`
+	FormEmail                     string `json:"form_email"`
+	FormEmailPlaceholder          string `json:"form_email_placeholder"`
+	FormCompany                   string `json:"form_company"`
+	FormCompanyPlaceholder        string `json:"form_company_placeholder"`
+	FormPhone                     string `json:"form_phone"`
+	FormPhonePlaceholder          string `json:"form_phone_placeholder"`
+	FormVisitorType               string `json:"form_visitor_type"`
+	FormVisitorTypePleaseSelect   string `json:"form_visitor_type_please_select"`
+	FormVisitorTypeContractor     string `json:"form_visitor_type_contractor"`
+	FormVisitorTypeVendor         string `json:"form_visitor_type_vendor"`
+	FormVisitorTypeGuest          string `json:"form_visitor_type_guest"`
+	FormMshaNumber                string `json:"form_msha_number"`
+	FormMshaNumberPlaceholder     string `json:"form_msha_number_placeholder"`
+	FormTruckNumber               string `json:"form_truck_number"`
+	FormTruckNumberPlaceholder    string `json:"form_truck_number_placeholder"`
+	FormPreferredLang             string `json:"form_preferred_lang"`
+	FormPreferredLangPleaseSelect string `json:"form_preferred_lang_please_select"`
+	FormPreferredLangEn           string `json:"form_preferred_lang_en"`
+	FormPreferredLangEs           string `json:"form_preferred_lang_es"`
+	AddTraineeButton              string `json:"add_trainee_button"`
+	GoBackButton                  string `json:"go_back_button"`
+	ContinueButton                string `json:"continue_button"`
+	FormNewHereText               string `json:"form_new_here_text"`
+	FormNewHereSubtext            string `json:"form_new_here_subtext"`
+	FormWelcomeBackText           string `json:"form_welcome_back_text"`
+	FormWelcomeBackSubtext        string `json:"form_welcome_back_subtext"`
+	FormTrainingExpiredText       string `json:"form_training_expired_text"`
+	FormTrainingWillExpireText    string `json:"form_training_will_expire_text"`
+	IsThisYouText                 string `json:"is_this_you_text"`
+	IsThisYouSubtext              string `json:"is_this_you_subtext"`
+	NotMeButton                   string `json:"not_me_button"`
+	ThisIsMeButton                string `json:"this_is_me_button"`
+	AnyOfTheseYouText             string `json:"any_of_these_you_text"`
+	AnyOfTheseYouSubtext          string `json:"any_of_these_you_subtext"`
+	NoneAreMeButton               string `json:"none_are_me_button"`
+	RestartVideo                  string `json:"restart_video"`
+	StartVideo                    string `json:"start_video"`
+	TraineeListText               string `json:"trainee_list_text"`
+	TraineeListSubtext            string `json:"trainee_list_subtext"`
+	StartTrainingButton           string `json:"start_training_button"`
+	AgreementCheckboxText         string `json:"agreement_checkbox_text"`
+	SignatureSubtext              string `json:"signature_subtext"`
+	ClearButton                   string `json:"clear_button"`
+	YourDoneText                  string `json:"your_done_text"`
+	YourDoneSubtext               string `json:"your_done_subtext"`
+	YourDoneSubtextCountdown      string `json:"your_done_subtext_countdown"` // there is a typo in the original file
+}
 
-type TextMessage struct{}
+// TextMessage represents a text message template that can be sent to recipients.
+type TextMessage struct {
+	ID         string    `json:"id"`
+	Title      string    `json:"title"`
+	Text       string    `json:"text"`
+	Type       string    `json:"type"`
+	Created    time.Time `json:"created"`
+	Updated    time.Time `json:"updated"`
+	LocationID string    `json:"location_id"`
+}
 
-type AutomaticTextMessage struct{}
-
-type Form struct{}
+// AutomaticTextMessage represents a scheduled text message to be sent to recipients.
+type AutomaticTextMessage struct {
+	ID                 string   `json:"id"`
+	RuleName           string   `json:"ruleName"`
+	ScheduleExpression string   `json:"scheduleExpression"`
+	Message            string   `json:"message"`
+	Recipients         []string `json:"recipients"`
+	Title              string   `json:"title"`
+	Active             bool     `json:"active"`
+	LocationID         string   `json:"location_id"`
+	DayOfWeek          string   `json:"dayOfWeek"`
+	DayOfMonth         string   `json:"dayOfMonth"`
+	Frequency          string   `json:"frequency"`
+	TimeToSend         string   `json:"timeToSend"`
+	RecipientType      string   `json:"recipientType"`
+	MessageID          string   `json:"message_id"`
+}
--- repository/repository.go
+++ repository/repository.go
@@ -2,16 +2,112 @@ package repository
 
 import "github.com/babykittenz/api-micro-util/models"
 
+// TraineeRepository provides methods to perform CRUD operations and specific queries on Trainee data.
 type TraineeRepository interface {
 	FindByID(id string) (*models.Trainee, error)
 	FindByEmail(email string) (*models.Trainee, error)
+	FindByPhone(phone string) (*models.Trainee, error)
+	FindByPhoneAndLocation(phone string, location string) (*models.Trainee, error)
+	FindByEmailAndLocation(email string, location string) (*models.Trainee, error)
+	FindByNames(firstName string, lastName string) (*models.Trainee, error)
+	FindByNamesAndLocation(firstName string, lastName string, location string) (*models.Trainee, error)
+	FindAll() ([]*models.Trainee, error)
+	FindAllByCompanyID(id string) ([]*models.Trainee, error)
+	FindAllByRegionID(id string) ([]*models.Trainee, error)
+	FindAllByLocationID(id string) ([]*models.Trainee, error)
 	Save(trainee *models.Trainee) error
 	Update(trainee *models.Trainee) error
 	Delete(id string) error
+	DeleteByEmail(email string) error
+	CompleteTraining(id string) error
+	Checkin(id string) error
+	Checkout(id string) error
 }
 
+// TrainingRepository defines the interface for interacting with Training data storage and management operations.
+type TrainingRepository interface {
+	FindByID(id string) (*models.Training, error)
+	FindAll() ([]*models.Training, error)
+	FindAllByCompanyID(id string) ([]*models.Training, error)
+	FindAllByRegionID(id string) ([]*models.Training, error)
+	FindAllByLocationID(id string) ([]*models.Training, error)
+	FindAllByTraineeID(id string) ([]*models.Training, error)
+	Save(training *models.Training) error
+	Update(training *models.Training) error
+	Delete(id string) error
+}
+
+// CheckinRepository defines a contract for managing check-in records in a storage system.
+// It provides methods for retrieving, creating, updating, and deleting check-in data.
+// The repository operates on models.Checkin entities for CRUD operations.
+type CheckinRepository interface {
+	FindByID(id string) (*models.Checkin, error)
+	FindAll() ([]*models.Checkin, error)
+	FindAllByCompanyID(id string) ([]*models.Checkin, error)
+	FindAllByRegionID(id string) ([]*models.Checkin, error)
+	FindAllByTraineeID(id string) ([]*models.Checkin, error)
+	Save(checkin *models.Checkin) error
+	Update(checkin *models.Checkin) error
+	Delete(id string) error
+}
+
+// CompanyRepository defines the methods required for managing company data within a persistence layer.
 type CompanyRepository interface {
 	FindByID(id string) (*models.Company, error)
 	FindAll() ([]*models.Company, error)
 	Save(company *models.Company) error
+	Update(company *models.Company) error
+	Delete(id string) error
+}
+
+// RegionRepository defines the interface for managing operations related to regions in the repository.
+// Provides methods to find, save, update, and delete regions.
+type RegionRepository interface {
+	FindByID(id string) (*models.Region, error)
+	FindAll() ([]*models.Region, error)
+	FindAllByCompanyID(id string) ([]*models.Region, error)
+	Save(region *models.Region) error
+	Update(region *models.Region) error
+	Delete(id string) error
+}
+
+// LocationRepository defines the interface for managing and executing operations on Location entities.
+// It provides methods to retrieve, save, update, and delete location data.
+type LocationRepository interface {
+	FindByID(id string) (*models.Location, error)
+	FindAll() ([]*models.Location, error)
+	FindAllByCompanyID(id string) ([]*models.Location, error)
+	FindAllByRegionID(id string) ([]*models.Location, error)
+	Save(location *models.Location) error
+	Update(location *models.Location) error
+	Delete(id string) error
+}
+
+// LanguageRepository defines methods to manage Language resources in storage.
+type LanguageRepository interface {
+	FindByID(id string) (*models.Language, error)
+	FindAll() ([]*models.Language, error)
+	Save(language *models.Language) error
+	Update(language *models.Language) error
+	Delete(id string) error
+}
+
+// TextMessageRepository defines methods to interact with and manage TextMessage data storage and retrieval.
+type TextMessageRepository interface {
+	FindByID(id string) (*models.TextMessage, error)
+	FindAll() ([]*models.TextMessage, error)
+	FindAllByLocationID(id string) ([]*models.TextMessage, error)
+	Save(textMessage *models.TextMessage) error
+	Update(textMessage *models.TextMessage) error
+	Delete(id string) error
+}
+
+// AutomaticTextMessageRepository defines the interface for managing AutomaticTextMessage data storage and retrieval.
+type AutomaticTextMessageRepository interface {
+	FindByID(id string) (*models.AutomaticTextMessage, error)
+	FindAll() ([]*models.AutomaticTextMessage, error)
+	FindAllByLocationID(id string) ([]*models.AutomaticTextMessage, error)
+	Save(automaticTextMessage *models.AutomaticTextMessage) error
+	Update(automaticTextMessage *models.AutomaticTextMessage) error
+	Delete(id string) error
 }
